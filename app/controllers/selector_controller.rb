023472cb8fd407515c776adb52902413a8294d5e
Ransom Byers <email": null>
2017-04-13 17:53:14 -0700

    Clean up unnecessary print statements

--- app/controllers/selector_controller.rb
+++ app/controllers/selector_controller.rb
@@ -2,3 +2,4 @@ post '/selectors' do
   @snack = Selector.pick_by_energy(params[:relative_energy])
   erb :'/snacks/show'
 end
+
--- app/models/selector.rb
+++ app/models/selector.rb
@@ -1,12 +1,7 @@
 class Selector
-  attr_reader :num_choices
 
   def self.pick_by_energy(relative_energy)
-    p "*" * 40
-    p "relative energy #{relative_energy}"
-    p "snack count #{Snack.all.count}"
     index = (relative_energy.to_f * Snack.all.count).floor
-    p "index #{index}"
     Snack.all.order('energy')[index]
   end
 end
