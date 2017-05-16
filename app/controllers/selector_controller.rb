095b50e6edd36723f7ffc9e2582c305b10e6bf88
Ransom <ransom.byers@gmail.com>
2017-05-15 18:08:41 -0700

    Remove Sinatra App

--- app/controllers/selector_controller.rb
+++
@@ -1,5 +0,0 @@
-post '/selectors' do
-  @snack = Selector.pick_by_energy(params[:relative_energy])
-  erb :'/snacks/show'
-end
-
--- app/models/selector.rb
+++
@@ -1,7 +0,0 @@
-class Selector
-
-  def self.pick_by_energy(relative_energy)
-    index = (relative_energy.to_f * Snack.all.count).floor
-    Snack.all.order('energy')[index]
-  end
-end
