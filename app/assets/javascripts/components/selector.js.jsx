61d2685096baa280231529b4a8ac5edd43b7e99b
Ransom <ransom.byers@gmail.com>
2017-06-01 16:44:58 -0700

    Move setting the list of available machines to the controller

--- app/assets/javascripts/components/selector.js.jsx
+++ app/assets/javascripts/components/selector.js.jsx
@@ -3,6 +3,8 @@ var MachineSelector = React.createClass({
     var optionsList = this.props.options.map(function(option) {
       return <li>{option}</li>
     });
+
+
     return (
       <div>
         <h3>Here is where you pick the machine</h3>
--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,4 +1,5 @@
 class MachinesController < ApplicationController
   def index
+    @machines_list = ['generic','conspire']
   end
 end
