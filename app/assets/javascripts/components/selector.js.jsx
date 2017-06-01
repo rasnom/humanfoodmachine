b8d2f1c99636b9da629c03b3ccd4ededd5ad202d
Ransom <ransom.byers@gmail.com>
2017-06-01 06:43:36 -0700

    Add options to the selector

--- app/assets/javascripts/components/selector.js.jsx
+++ app/assets/javascripts/components/selector.js.jsx
@@ -1,5 +1,13 @@
 var MachineSelector = React.createClass({
   render: function() {
-    return <h3>Here is where you pick the machine</h3>
+    var optionsList = this.props.options.map(function(option) {
+      return <li>{option}</li>
+    });
+    return (
+      <div>
+        <h3>Here is where you pick the machine</h3>
+        <ul>{optionsList}</ul>
+      </div>
+    )
   }
 });
