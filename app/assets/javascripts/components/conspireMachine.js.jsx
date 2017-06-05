3ec2babe7b1269cb80c2437a3e1f229a9f855114
Ransom <ransom.byers@gmail.com>
2017-06-05 16:28:36 -0700

    Add single person interface for Conspire machine

--- app/assets/javascripts/components/conspireMachine.js.jsx
+++ app/assets/javascripts/components/conspireMachine.js.jsx
@@ -1,4 +1,9 @@
 var ConspireMachine = React.createClass({
+  getInitialState: function() {
+    return {
+      selection: null
+    }
+  },
 
   getDefaultProps: function() {
     return {
@@ -7,11 +12,50 @@ var ConspireMachine = React.createClass({
     }
   },
 
+  handleSelection: function(e) {
+    this.setState({selection: e.target.value})
+  },
+
+  displayOptions: function() {
+    that = this;
+    return (
+      <ul>
+        {this.props.stock.map( function(item) {
+          return (
+            <li><button value={item} onClick={that.handleSelection}>
+              {item}
+            </button></li>
+          )
+        })}
+      </ul>
+    )
+  },
+
+  handleSubmit: function() {
+    if (this.state.selection != null) {
+      alert ("you got a " + this.state.selection)
+    }
+  },
+
+  submitter: function() {
+    return (<button onClick={this.handleSubmit}>submit</button>)
+  },
+
+  displayInterface: function() {
+    return (
+      <div>
+        {this.displayOptions()}
+        {this.submitter()}
+      </div>
+    )
+  },
+
   render: function() {
     return (
       <div>
         <h3>{this.props.title}</h3>
         <p>{this.props.description}</p>
+        {this.displayInterface()}
       </div>
     );
   }
